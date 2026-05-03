10422737bf5f3155a7ac5e36cc3c29571707e0ce
Gangadhar <zenitsuig65@gmail.com>
2026-05-03 12:13:09 +0530

    implemented base62encoder util and urlrepository

---
+++ url-shortner/src/main/java/com/example/url_shortner/repository/UrlRepository.java
@@ -0,0 +1,33 @@
+package com.example.url_shortner.repository;
+
+import com.example.url_shortner.model.UrlMapping;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface UrlRepository extends JpaRepository<UrlMapping, Long> {
+
+    Optional<UrlMapping> findByShortKey(String shortKey);
+    boolean existsByShortKey(String shortKey);
+    //fetch only original url
+    @Query("""
+            SELECT u.originalUrl
+            FROM UrlMapping u
+            WHERE u.shorKey = :shortKey
+""")
+    Optional<String> findOriginalUrlByShortKey(@Param("shortKey")String shortKey);
+
+    //expiration check useful for redirect flow
+    @Query(
+            """
+            SELECT u.originalUrl
+            FROM UrlMapping u
+            WHERE u.shortKey = :shortKey
+                AND (u.expiresAt IS NULL OR u.expiresAt > CURRENT_TIMESTAMP)
+"""
+    )
+    Optional<String> findValidOriginalUrl(@Param("shortKey") String shortKey);
+
+}
